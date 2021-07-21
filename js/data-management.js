f4c6921e6a15c9ab4cd7f8927531eaf46fb58caf
AlessandroVe <aveneziano536@gmail.com>
2021-07-22 00:56:29 +0200

    .

--- js/data-management.js
+++ js/data-management.js
@@ -1,6 +1,7 @@
 new Vue({
     el: '#container-master',
     data:{
+        ArrayWishList:[],
         ArrayOfNewItem:[
             {
                 name:"Uova biologiche ",
@@ -27,10 +28,102 @@ new Vue({
                 rate:4, 
             },
         ],
+        ArrayOfDiscountedProducts:[
+            {
+                name:"Anguria",
+                cost:10.99,
+                image:"anguria.jpg",
+                rate:4,
+            },
+            {
+                name:"Polpa di pomodoro",
+                cost:1.50,
+                image:"polpa_di_pomodoro.jpg",
+                rate:3,
+            },
+            {
+                name:"Arance",
+                cost:1.70,
+                image:"arance.jpg",
+                rate:4,
+            },
+            {
+                name:"Lenticchie",
+                cost:1.00,
+                image:"lenticchie.jpg",
+                rate:4, 
+            },
+        ],
+        ArrayOfPopularItem:[
+            {
+                name:"Banana",
+                cost:1.99,
+                image:"banana.jpg",
+                rate:4,
+            },
+            {
+                name:"Cipolla",
+                cost:0.85,
+                image:"cipolla.jpg",
+                rate:3,
+            },
+            {
+                name:"Toast",
+                cost:2.30,
+                image:"toast.jpg",
+                rate:4,
+            },
+            {
+                name:"Polpa di pomodoro",
+                cost:1.50,
+                image:"polpa_di_pomodoro.jpg",
+                rate:3,
+            },
+        ],
+        ArrayOfTopRatedItem:[
+            {
+                name:"Banana",
+                cost:1.99,
+                image:"banana.jpg",
+                rate:4,
+            },
+            {
+                name:"Cipolla",
+                cost:0.85,
+                image:"cipolla.jpg",
+                rate:3,
+            },
+            {
+                name:"Mela",
+                cost:2.50,
+                image:"mela.jpg",
+                rate:3.5,
+            },
+            {
+                name:"Carota",
+                cost:2.50,
+                image:"carota.jpg",
+                rate:4,
+            },
+            
+
+        
+
+        ],
     },
     created: function () {
        console.log(this.ArrayOfNewItem)
     },
     methods:{
+        AddToWishList:function(elementoOfArray){
+            if(!this.ArrayWishList.includes(elementoOfArray)){
+                if(this.ArrayWishList.length < 4){
+                    this.ArrayWishList.push(elementoOfArray)
+                }
+            }
+        },
+        RemoveToWishList:function(indexOfElement){
+            this.ArrayWishList.splice(indexOfElement,1)
+        }
     },  
   })
